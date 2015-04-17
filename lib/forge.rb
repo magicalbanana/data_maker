316d9eb396702b3ab91576867dae449d3f5fb10e
Kareem Gan <kareemgan@gmail.com>
2015-04-16 23:12:36 -0500

    Forge or should I say forgery. Added some tests too.

---
+++ lib/forge.rb
@@ -0,0 +1,63 @@
+module Forge
+  VERSION = "1.0.0"
+
+  require 'forge/utilities/array_utilities'
+  require 'forge/utilities/module_utilities'
+
+  extend ModuleUtilities
+
+  BASE_LIB_PATH = File.expand_path("..", __FILE__)
+
+  LETTERS = [*'a'..'z']
+
+  HEX = %w(0 1 2 3 4 5 6 7 8 9 A B C D E F)
+
+  def self.hexify(*masks)
+    begin
+      if valid_mask?(*masks, /#/)
+        masks.flatten.sample.gsub(/#/) { HEX.sample }
+      end
+    rescue
+      return false
+    end
+  end
+
+  def self.numerify(*masks)
+    begin
+      if valid_mask?(*masks, /#/)
+        masks.flatten.sample.gsub(/#/) { rand(10).to_s }
+      end
+    rescue
+      return false
+    end
+  end
+
+  def self.letterify(*masks)
+    begin
+      if valid_mask?(*masks, /\?/)
+        masks.flatten.sample.gsub(/\?/) { LETTERS.sample }
+      end
+    rescue
+      return false
+    end
+  end
+
+  def self.alphanumerify(masks)
+    letterify(numerify(masks))
+  end
+
+  # Load all constants.
+  Dir["#{BASE_LIB_PATH}/forge/*.rb"].sort.each do |f|
+    require "forge/#{File.basename(f, '.rb')}"
+  end
+
+  def self.valid_mask?(*masks, match_with)
+    masks.each do |mask|
+      unless mask.match(match_with)
+        raise ArgumentError, "You must pass a #{match_with} sign sir!"
+      end
+    end
+  end
+
+  private_class_method :valid_mask?
+end
---
+++ spec/lib/forge/forge_spec.rb
@@ -0,0 +1,138 @@
+require 'spec_helper'
+
+RSpec.describe Forge do
+  let(:masks) { value }
+
+  describe "#VERSION" do
+    # For each iteration of a version this would always get changed.
+    let(:current_version) { "1.0.0" }
+
+    it { expect(described_class).to be_const_defined(:VERSION) }
+    it { expect(described_class::VERSION).to eq(current_version) }
+  end
+
+  describe "#self.hexify" do
+    let(:regex) { /\b[a-zA-Z0-9]{3}\b/ }
+
+    context "when valid_masks? is true" do
+      let(:value) { "### hello ###" }
+
+      it "returns a hexed value" do
+        expect(described_class.hexify(*masks)).to match(regex)
+      end
+
+      context "when passed an array value" do
+        let(:value) { ["hello ###", "my ### is ###"] }
+
+        it "returns a hexed value" do
+          expect(described_class.hexify(*masks)).to match(regex)
+        end
+      end
+    end
+
+    context "when valid_masks? raises an error" do
+      let(:value) { "hello my name is banana" }
+
+      it "returns false" do
+        expect(described_class.hexify(*masks)).to be_falsey
+      end
+    end
+  end
+
+  describe "#self.numerify" do
+    let(:regex) { /\b[0-9]{3}\b/ }
+
+    context "when valid_masks? is true" do
+      let(:value) { "my phone number is ###" }
+
+      it "returns a numeric value" do
+        expect(described_class.numerify(*masks)).to match(regex)
+      end
+
+      context "when passed an array value" do
+        let(:value) { ["my phone number is ###",  "unit no. ###"] }
+
+        it "returns a numeric value" do
+          expect(described_class.numerify(*masks)).to match(regex)
+        end
+      end
+    end
+
+    context "when valid_masks? raises an error" do
+      let(:value) { ["my phone number is 3333",  "unit no. seven"] }
+
+      it "returns false" do
+        expect(described_class.numerify(*masks)).to be_falsey
+      end
+    end
+  end
+
+  describe "#self.letterify" do
+    let(:regex) { /\b[a-z]{3}\b/ }
+
+    context "when valid_masks? is true" do
+      let(:value) { "convert me to letters ???" }
+
+      it "returns alphabetic characters" do
+        expect(described_class.letterify(*masks)).to match(regex)
+      end
+
+      context "when passed an array value" do
+        let(:value) { ["convert me ???", "me too ???"] }
+
+        it "returns alphabetic characters" do
+          expect(described_class.letterify(*masks)).to match(regex)
+        end
+      end
+    end
+
+    context "when valid_masks? raises an error" do
+      let(:value) { "convert me to letters ###" }
+
+      it "returns false" do
+        expect(described_class.letterify(*masks)).to be_falsey
+      end
+    end
+  end
+
+  describe "#self.alphanumerify" do
+    let(:regex) { /\b[a-zA-Z0-9]{3}\b/ }
+
+    context "when valid_masks? is true" do
+      let(:value) { "alphanumerify me ##?" }
+
+      it "returns alphanumeric characters" do
+        expect(described_class.alphanumerify(*masks)).to match(regex)
+      end
+    end
+
+    context "when valid_masks? raises an error" do
+      let(:value) { "I am not correct" }
+
+      it "returns false" do
+        expect(described_class.alphanumerify(*masks)).to be_falsey
+      end
+    end
+  end
+
+  # PRIVATE
+  describe "#valid_mask?" do
+    context "if mask.match(match_with) is not nil" do
+      let(:masks) { ["Apt. #" "##",  "Banana Street No. ###"] }
+      let(:match_with) { /#/ }
+
+      it "returns true" do
+        expect(described_class.send(:valid_mask?, *masks, match_with)).to be_truthy
+      end
+    end
+
+    context "if mask is not equal to '#'" do
+      let(:masks) { ["###", "#" "abc @", "!"] }
+      let(:match_with) { /\?/ }
+
+      it "raises an error" do
+        expect { described_class.send(:valid_mask?, *masks, match_with) }.to raise_error
+      end
+    end
+  end
+end
